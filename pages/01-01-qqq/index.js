72d7a5300dc03f112dabfe0246d6013713d3ffbd
YunkiJung <100543183+YunkiJung@users.noreply.github.com>
2023-03-22 18:41:07 -0400

    day 1

---
+++ pages/01-01-qqq/index.js
@@ -0,0 +1,10 @@
+export default function Qqq(){
+    
+    return(
+        <div>
+            <div>철수</div>
+            <button>클릭하세요!!</button>
+        </div>
+    )
+
+}
---
+++ pages/01-02-emotion/index.js
@@ -0,0 +1,16 @@
+import {Email, EmailInput} from '../../styles/emotion'
+
+export default function EmotionPage(){
+    
+    // Javascript Area
+
+    return(
+        <div>
+            <Email>Email</Email>
+            <EmailInput type="text" />
+            <button>Click!!</button>
+            <img src='/vercel.svg' />
+        </div>
+    )
+
+}
